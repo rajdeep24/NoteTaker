b866bac12f6a4ebac817e186d8e5f1e7210f6b62
Developer Raj <rajdeepkakar@gmail.com>
2020-10-02 22:14:09 -0400

    Established server dependencies, defined express configration, assigned port number, configured listerner, and created intial route to index.html.

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 //Dependencies
-
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 //Express Configurations
+const app = express();
 
 //Define a PORT
-
+const PORT = process.env.PORT || 3001;
 //Middle Ware (optional)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 //Listener
-
+app.listen(PORT, function () {
+	console.log("App listening on PORT: " + PORT);
+});
 //Routes
+app.get("/", function (req, res) {
+	res.json(path.join(__dirname, "./public"));
+});
 
 //Home page
 
