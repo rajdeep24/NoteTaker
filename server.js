43d36046f200d18943dbba00a875528293f30bb6
Developer Raj <rajdeepkakar@gmail.com>
2020-10-03 02:23:39 -0400

    Added the GET API route to GET  that reads the JSON file using file system and returns the parsed JSON data to the application.

--- server.js
+++ server.js
@@ -16,74 +16,28 @@ app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, function () {
 	console.log("App listening on PORT: " + PORT);
 });
-//Routes
+///////////////////////HTML ROUTES///////////////////////
 //Home page
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "./"));
+	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 //Notes page
 app.get("/notes", function (req, res) {
 	res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
-//GET saved notes
-app.get("/api/notes", function (req, res) {
+///////////////////////API ROUTES///////////////////////
+//GET saved notes ( * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.)
+app.get("/api/notes", (req, res) => {
 	fs.readFile("./db/db.json", "utf8", (err, data) => {
-		if (err) {
-			throw err;
-		}
+		if (err) throw err;
 		res.json(JSON.parse(data));
 	});
 });
+
+//* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 //Add a new note
-app.post("/api/notes", function (req, res) {
-	if (req.body.title && req.body.text) {
-		const noteObject = req.body;
-
-		fs.readFile("./db/db.json", "utf8", (err, data) => {
-			if (err) {
-				throw err;
-			}
-
-			const noteArray = JSON.parse(data);
-			noteObject.id = noteArray.length + 1;
-			noteArray.push(noteObject);
-
-			fs.writeFile("./db/db.json", JSON.stringify(noteArray), (err, data) => {
-				if (err) {
-					throw err;
-				}
-			});
-			res.json(JSON.parse(data));
-		});
-	}
-});
 
 //Delete a note
-app.delete("/api/notes/:id", function (req, res) {
-	const id = req.params.id;
 
-	fs.readFile("./db/db.json", "utf8", (err, data) => {
-		if (err) {
-			throw err;
-		}
-
-		const noteArray = JSON.parse(data);
-
-		const newNoteArray = noteArray.filter((item) => {
-			return item.id != id;
-		});
-
-		fs.writeFile("./db/db.json", JSON.stringify(newNoteArray), (err, data) => {
-			if (err) {
-				throw err;
-			}
-		});
-		res.json(JSON.parse(data));
-	});
-});
 //Go to home page
-app.get("*", function (req, res) {
-	res.sendFile(path.join(__dirname, "/public/index.html"));
-});
