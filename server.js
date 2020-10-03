d98eb2ed2e353dcc7198c1447b0663f86084d58b
Developer Raj <rajdeepkakar@gmail.com>
2020-10-02 22:26:00 -0400

    Updated homepage GET method to send the response path to join the directory name with the index.html path.

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ app.listen(PORT, function () {
 	console.log("App listening on PORT: " + PORT);
 });
 //Routes
-app.get("/", function (req, res) {
-	res.json(path.join(__dirname, "/public/index.html"));
-});
-
 //Home page
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 //Notes page
 
