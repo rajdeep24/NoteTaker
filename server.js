086048c51b7617fb8d7032f4335275d92ca05fa6
Developer Raj <rajdeepkakar@gmail.com>
2020-10-02 23:44:13 -0400

    removed and added the public folder

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.listen(PORT, function () {
 //Routes
 //Home page
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "/public/index.html"));
+	res.sendFile(path.join(__dirname, "./"));
 });
 
 //Notes page
